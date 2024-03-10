8929233844d137a8abf180829a49522345530ebb
nickfrey22 <88825645+nickfrey22@users.noreply.github.com>
2024-03-10 15:31:47 -0700

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Optional, Set, Tuple, Union
 from cereal import car
 from panda.python import uds
 from openpilot.common.conversions import Conversions as CV
-from openpilot.selfdrive.car import dbc_dict, Angle_Rate_Limit
+from openpilot.selfdrive.car import dbc_dict, AngleRateLimit
 from openpilot.selfdrive.car.docs_definitions import CarFootnote, CarHarness, CarInfo, CarParts, Column
 from openpilot.selfdrive.car.fw_query_definitions import FwQueryConfig, Request, p16
 
